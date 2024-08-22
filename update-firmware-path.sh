35049e780bce591e5ccf50a771fec0a8f1a87ea2
Vitaliy Emporopulo <vemporop@redhat.com>
2024-08-22 14:28:36 +0300

    Fail if yq isn't installed

--- update-firmware-path.sh
+++ update-firmware-path.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 set -e
+set -o pipefail
+
+yq --version
 
 modified_value=$(oc get configmap -n openshift-kmm kmm-operator-manager-config -o "jsonpath={.data['controller_config\.yaml']}" | yq -y '.worker += {"setFirmwareClassPath": "/var/lib/firmware"}' | sed -z 's/\n/\\n/g')
 oc patch configmap kmm-operator-manager-config -n openshift-kmm --type=merge -p="{\"data\": {\"controller_config.yaml\":\"${modified_value}\"}}"
